6e4e865c73149d6121583d956f283a0496ee627e
Ayocoding12 <gbemialihassan@gmail.com>
2022-09-08 14:20:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * * main - Write '"Programming is like building a multilingual puzzle'
  * *Return: Always 0 (success)
- * */
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
