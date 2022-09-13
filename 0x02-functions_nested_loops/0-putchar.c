8b4392e6eaa1f309868500361131f314ae33418f
Ayocoding12 <111683116+Ayocoding12@users.noreply.github.com>
2022-09-13 11:12:38 +0200

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,19 @@
 
 /**
  * main - prints putchar
- * Return: always 0
+ *  Return: always 0
  */
 int main(void)
 {
-		_putchar('_');
-			_putchar('p');
-				_putchar('u');
-					_putchar('t');
-						_putchar('c');
-							_putchar('h');
-								_putchar('a');
-									_putchar('r');
-										_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-											return (0);
-}
+	return (0);
+}									
