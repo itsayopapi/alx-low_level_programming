6c3b1aec8c83256efca4f9acc3e74939daa18dcc
Ayo <111683116+Ayocoding12@users.noreply.github.com>
2022-09-15 00:34:40 +0200

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,19 +26,19 @@ void print_times_table(int n)
 		{
 		_putchar(32);
 		_putchar(32);
-		_putchar(op + 48);
+		_putchar(op + 48)
 		}
 		else if (op <= 99)
 		{
 		_putchar(32);
 		_putchar((op / 10) + 48);
-		_putchar((op % 10) + 48);
+		_putchar((op / 10) + 48);
 		}
 		else
 		{
 		_putchar(((op / 100) % 10) + 48);
 		_putchar(((op / 10) % 10) + 48);
-		_putchar((op % 10) + 48);
+		_putchar(((op % 10) + 48);
 		}
 	}
 	_putchar('\n');
