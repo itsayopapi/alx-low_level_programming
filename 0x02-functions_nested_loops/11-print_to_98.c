4546fb7887574f39bae1bdd3a62fc4a91e0d3945
Ayo <111683116+Ayocoding12@users.noreply.github.com>
2022-09-15 00:20:55 +0200

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_to_98 - print n to 98 counts
+ *
+ * @n: input
+ * 
+ */
+
+void print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+		for (count = n; count > 98; --count)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
+	printf("98\n")
+}
