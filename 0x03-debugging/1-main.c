393148a4054aed18a52f13130c6e000d88e0022f
Ayo <111683116+Ayocoding12@users.noreply.github.com>
2022-09-15 13:25:14 +0200

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (1 < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf( "Infinite loop avoided! \\0/\n");
+
+	return (0);
+}
