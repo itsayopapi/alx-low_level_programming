64ef92f6cc55de173194b651ee3036a3cd3c536d
Ayo <111683116+Ayocoding12@users.noreply.github.com>
2022-09-22 05:15:11 +0200

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,51 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * * print_times_table - Print the n times table
- *
- * @n: number times table table (0 < n<= 15)
- *
- * Return: no return
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
  */
 
 void print_times_table(int n)
 {
-	int a, b, op;
 
-	if (n >= 0 && n <= 15)
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+	return;
+
+while (a <= n)
+{
+	for (b = 0; b <= n; b++)
 	{
-	for (a = 0; a <= n; a++)
-	{
-	_putchar(48);
-	for (b = 1; b <= n; b++)
-	{
-		op = a * b;
-		_putchar(44);
-		_putchar(32);
-		if (op <= 9)
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
 		{
-		_putchar(32);
-		_putchar(32);
-		_putchar(op + 48)
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
 		}
-		else if (op <= 99)
+		else if (rep < 100)
 		{
-		_putchar(32);
-		_putchar((op / 10) + 48);
-		_putchar((op / 10) + 48);
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
 		}
 		else
 		{
-		_putchar(((op / 100) % 10) + 48);
-		_putchar(((op / 10) % 10) + 48);
-		_putchar(((op % 10) + 48);
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	_putchar('\n');
-	}
-	}
+	a++;
+}
 }
