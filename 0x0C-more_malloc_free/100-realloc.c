e1ff9a134fe7399b637bfbf952a3088bb877748a
Gorilla <111683116+Ayocoding12@users.noreply.github.com>
2022-10-06 10:46:53 +0200

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _realloc - change the size and copy the content
+ * @ptr: malloc pointer to reallocate
+ * @old_size: old number of bytes
+ * @new_size: new number of Bytes
+ * Return: nothing
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p = NULL;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (!p)
+			return (NULL);
+		return (p);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		if (!p)
+			return (NULL);
+		for (i = 0; i < old_size; i++)
+			p[i] = *((char *)ptr + i);
+		free(ptr);
+	}
+	else
+	{
+		p = malloc(new_size);
+		if (!p)
+			return (NULL);
+		for (i = 0; i < new_size; i++)
+			p[i] = *((char *)ptr + i);
+		free(ptr);
+	}
+	return (p);
+}
