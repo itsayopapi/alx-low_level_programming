37722c371926bd52e5f4971eebb000fe60576d18
Gorilla <111683116+Ayocoding12@users.noreply.github.com>
2022-10-10 13:50:22 +0200

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,3 +1,17 @@
-#ifndef SIZE
-#define SIZE 1024
-#endif
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+}
